752a5abf6b1471e66b33526ec7064d825ff57d06
VisiKard MacBook Pro <visikardmacbookpro@VisiKards-MacBook-Pro.local>
2013-04-02 10:19:03 +0700

    Initial Commit

---
+++ PassbookSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PassbookSample
+//
+//  Created by VisiKard MacBook Pro on 4/2/13.
+//  Copyright (c) 2013 VisiKard MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VKardAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VKardAppDelegate class]));
+    }
+}
